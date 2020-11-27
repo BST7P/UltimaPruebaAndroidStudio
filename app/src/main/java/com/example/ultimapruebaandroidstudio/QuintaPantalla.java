8d32337775171349ea5ccbabe0fdfbdd6eea5e5f
bstp <bstautas@iemsantateresita.edu.co>
2020-11-27 18:36:23 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/ultimapruebaandroidstudio/QuintaPantalla.java
@@ -0,0 +1,14 @@
+package com.example.ultimapruebaandroidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuintaPantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quinta_pantalla);
+    }
+}
---
+++ app/src/main/java/com/example/ultimapruebaandroidstudio/TERCERAPANTALLA.java
@@ -0,0 +1,14 @@
+package com.example.ultimapruebaandroidstudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TERCERAPANTALLA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t_e_r_c_e_r_a_p_a_n_t_a_l_l);
+    }
+}
